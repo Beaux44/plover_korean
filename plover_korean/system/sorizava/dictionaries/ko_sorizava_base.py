5e3e6b6a9c33c49eb79a36e25c5599d1b27a3618
Sammi Ta <sammi@sammdot.ca>
2025-05-13 23:29:19 -0700

    Sorizava syllable dictionary

--- plover_korean/system/sorizava/dictionaries/ko_sorizava_base.py
+++ plover_korean/system/sorizava/dictionaries/ko_sorizava_base.py
@@ -1,121 +1,140 @@
 """Core functionality for the Sorizava-based Korean stenography system."""
 
 from typing import Tuple, List
+from plover.system import Stroke
 
 import hgtk
 
-# TODO: from plover_korean.system.sorizava.util import get_stroke_groups
-
-
 LONGEST_KEY = 1
-OPERATOR_ATTACH = '{^}'
+OPERATOR_ATTACH = "{^}"
 
-INITIAL = {
-    'ㄱ': 'ㄱ',
-    'ㄲ': 'ㄱㅎ',
-    'ㄴ': 'ㄴ',
-    'ㄷ': 'ㄷ',
-    'ㄸ': 'ㄷㄹ',
-    'ㄹ': 'ㄹ',
-    'ㅁ': 'ㅁ',
-    'ㅂ': 'ㅂ',
-    'ㅃ': 'ㅂㄱ',
-    'ㅅ': 'ㅅ',
-    'ㅆ': 'ㅅㅁ',
-    'ㅇ': '',
-    'ㅈ': 'ㅈ',
-    'ㅉ': 'ㅈㄴ', # TODO: What is this actually? Best guess
-    'ㅊ': 'ㅊ',
-    'ㅋ': 'ㅋ', # TODO: Which ㅋ? There are two.
-    'ㅌ': 'ㅌ',
-    'ㅍ': 'ㅍ',
-    'ㅎ': 'ㅎ'
+INITIALS = {
+  Stroke("ㄱ"): "ㄱ",
+  Stroke("ㄱㅎ"): "ㄲ",
+  Stroke("ㄴ"): "ㄴ",
+  Stroke("ㄷ"): "ㄷ",
+  Stroke("ㄷㄹ"): "ㄸ",
+  Stroke("ㄹ"): "ㄹ",
+  Stroke("ㅁ"): "ㅁ",
+  Stroke("ㅂ"): "ㅂ",
+  Stroke("ㅂㄱ"): "ㅃ",
+  Stroke("ㅅ"): "ㅅ",
+  Stroke("ㅅㅁ"): "ㅆ",
+  Stroke(""): "ㅇ",
+  Stroke("ㅈ"): "ㅈ",
+  Stroke("ㅈㄴ"): "ㅉ",
+  Stroke("ㅊ"): "ㅊ",
+  Stroke("ㅋ"): "ㅋ",
+  Stroke("ㅌ"): "ㅌ",
+  Stroke("ㅍ"): "ㅍ",
+  Stroke("ㅎ"): "ㅎ",
 }
 
-MEDIAL = {
-    'ㅏ': 'ㅏ',
-    'ㅐ': 'ㅏㅣ',
-    'ㅑ': 'ㅏㅡ',
-    'ㅒ': 'ㅏㅡㅓ', # TODO: What is this actually? Best guess
-    'ㅓ': 'ㅓ',
-    'ㅔ': 'ㅓㅣ',
-    'ㅕ': 'ㅡㅓ',
-    'ㅖ': 'ㅏㅓㅣ',
-    'ㅗ': 'ㅗ',
-    'ㅘ': 'ㅗㅏ',
-    'ㅙ': 'ㅗㅏㅣ',
-    'ㅚ': 'ㅗㅣ',
-    'ㅛ': 'ㅗㅡ',
-    'ㅜ': 'ㅜ',
-    'ㅝ': 'ㅜㅓ',
-    'ㅞ': 'ㅜㅓㅣ',
-    'ㅟ': 'ㅜㅣ',
-    'ㅠ': 'ㅜㅡ',
-    'ㅡ': 'ㅡ',
-    'ㅢ': 'ㅢ',
-    'ㅣ': 'ㅣ'
+VOWELS = {
+  Stroke("ㅏ"): "ㅏ",
+  Stroke("ㅏㅣ"): "ㅐ",
+  Stroke("ㅏㅡ"): "ㅑ",
+  Stroke("ㅏㅓ"): "ㅒ",
+  Stroke("ㅓ"): "ㅓ",
+  Stroke("ㅓㅣ"): "ㅔ",
+  Stroke("ㅡㅓ"): "ㅕ",
+  Stroke("ㅏㅓㅣ"): "ㅖ",
+  Stroke("ㅗ"): "ㅗ",
+  Stroke("ㅗㅏ"): "ㅘ",
+  Stroke("ㅗㅏㅣ"): "ㅙ",
+  Stroke("ㅗㅣ"): "ㅚ",
+  Stroke("ㅗㅡ"): "ㅛ",
+  Stroke("ㅜ"): "ㅜ",
+  Stroke("ㅜㅓ"): "ㅝ",
+  Stroke("ㅜㅓㅣ"): "ㅞ",
+  Stroke("ㅜㅣ"): "ㅟ",
+  Stroke("ㅜㅡ"): "ㅠ",
+  Stroke("ㅡ"): "ㅡ",
+  Stroke("ㅢ"): "ㅢ",
+  Stroke("ㅣ"): "ㅣ",
 }
 
-FINAL = {
-    '': '',
-    'ㄱ': 'ㄱ',
-    'ㄲ': 'ㄲ',
-    'ㄳ': 'ㄱㅅ',
-    'ㄴ': 'ㄴ',
-    'ㄵ': 'ㄴㅈ',
-    'ㄶ': 'ㅎㄴ',
-    'ㄷ': 'ㄷ',
-    'ㄹ': 'ㄹ',
-    'ㄺ': 'ㄱㄹ',
-    'ㄻ': 'ㄹㅁ',
-    'ㄼ': 'ㄹㅂ',
-    'ㄽ': 'ㄹㅅ',
-    'ㄾ': 'ㅌㄹ',
-    'ㄿ': 'ㅍㄹ',
-    'ㅀ': 'ㅎㄹ',
-    'ㅁ': 'ㅁ',
-    'ㅂ': 'ㅂ',
-    'ㅄ': 'ㅅㅂ',
-    'ㅅ': 'ㅅ',
-    'ㅆ': 'ㅆ',
-    'ㅇ': 'ㅇ',
-    'ㅈ': 'ㅈ',
-    'ㅊ': 'ㅊ',
-    'ㅋ': 'ㅎㄱ', # TODO: What is this? Best guess
-    'ㅌ': 'ㅌ',
-    'ㅍ': 'ㅍ',
-    'ㅎ': 'ㅎ'
+FINALS = {
+  Stroke(""): "",
+  Stroke("-ㄱ"): "ㄱ",
+  Stroke("-ㄲ"): "ㄲ",
+  Stroke("-ㄱㅅ"): "ㄳ",
+  Stroke("-ㄴ"): "ㄴ",
+  Stroke("-ㄴㅈ"): "ㄵ",
+  Stroke("-ㅎㄴ"): "ㄶ",
+  Stroke("-ㄷ"): "ㄷ",
+  Stroke("-ㄹ"): "ㄹ",
+  Stroke("-ㄱㄹ"): "ㄺ",
+  Stroke("-ㄹㅁ"): "ㄻ",
+  Stroke("-ㄹㅂ"): "ㄼ",
+  Stroke("-ㄹㅅ"): "ㄽ",
+  Stroke("-ㅌㄹ"): "ㄾ",
+  Stroke("-ㅍㄹ"): "ㄿ",
+  Stroke("-ㅎㄹ"): "ㅀ",
+  Stroke("-ㅁ"): "ㅁ",
+  Stroke("-ㅂ"): "ㅂ",
+  Stroke("-ㅅㅂ"): "ㅄ",
+  Stroke("-ㅅ"): "ㅅ",
+  Stroke("-ㅆ"): "ㅆ",
+  Stroke("-ㅇ"): "ㅇ",
+  Stroke("-ㅈ"): "ㅈ",
+  Stroke("-ㅊ"): "ㅊ",
+  Stroke("-ㅋ"): "ㅋ",
+  Stroke("-ㅌ"): "ㅌ",
+  Stroke("-ㅍ"): "ㅍ",
+  Stroke("-ㅎ"): "ㅎ",
 }
+
+INITIAL_KEYS = Stroke("ㅊㅌㅋㅂㅍㅅㄷㅈㄱㅁㄹㄴㅎ")
+VOWEL_KEYS = Stroke("ㅢㅗㅏㅜㅡㅓㅣ")
+FINAL_KEYS = Stroke("-ㅋㄲㅎㅌㅊㅍㄱㄴㄹㅅㅂㅆㅇㅁㄷㅈ")
 
 
 def lookup(strokes: Tuple[str]) -> str:
-    """Gets the text that the provided strokes would output.
+  """Gets the text that the provided strokes would output.
 
-    Args:
-        strokes: A tuple of strokes to look up text for.
+  Args:
+      strokes: A tuple of strokes to look up text for.
 
-    Returns:
-        The text text output for the stroke.
+  Returns:
+      The text text output for the stroke.
 
-    Raises:
-        KeyError: The lookup failed to find any matching text.
-    """
+  Raises:
+      KeyError: The lookup failed to find any matching text.
+  """
+  if len(strokes) != LONGEST_KEY:
+    raise KeyError
 
-    if len(strokes) != LONGEST_KEY:
-        raise KeyError()
+  stroke = Stroke(strokes[0])
+  initial, vowel, final = (
+    stroke & INITIAL_KEYS,
+    stroke & VOWEL_KEYS,
+    stroke & FINAL_KEYS,
+  )
+  initial, vowel, final = (
+    INITIALS.get(initial, ""),
+    VOWELS.get(vowel),
+    FINALS.get(final, ""),
+  )
+  if not vowel:
+    raise KeyError
 
-    raise KeyError()
+  try:
+    hangul = hgtk.letter.compose(initial, vowel, final)
+    return f"{OPERATOR_ATTACH}{hangul}{OPERATOR_ATTACH}"
+  except:
+    raise KeyError
 
 
 def reverse_lookup(text: str) -> List[Tuple[str]]:
-    """Gets the possible strokes that would result in the provided text.
+  """Gets the possible strokes that would result in the provided text.
 
-    Args:
-        text: The text to look up strokes for.
+  Args:
+      text: The text to look up strokes for.
 
-    Returns:
-        A list of stroke tuples. An empty list will be returned if no possible
-        strokes were found.
-    """
+  Returns:
+      A list of stroke tuples. An empty list will be returned if no possible
+      strokes were found.
+  """
 
-    return []
+  return []
